028cbc863aa97d0ba00bc66868e4457e2c128515
matthiasbroske <tias@broske.com>
2024-01-20 21:22:40 -0600

    add fixed header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,20 +1,32 @@
 let appHeader = `
-    <div class="inner">
-        <a href="index.html" class="logo magic-tile">
-            <span class="symbol">
-                <img class="active-tile" src="images/logo.gif"/>
-                <img class="static-tile" src="images/logo.png"/>
-            </span>
-            <span class="title">Home</span>
-        </a>
-        <nav>
-            <ul>
-                <li><a href="#menu">Menu</a></li>
-            </ul>
-        </nav>
+<div class="navbar">
+    <a href="index.html" class="logo magic-tile">
+        <span class="symbol">
+            <img class="active-tile" src="images/logo.gif"/>
+            <img class="static-tile" src="images/logo.png"/>
+        </span>
+        <span class="title">Home</span>
+    </a>
+    <div class="dropdown">
+        <a>Projects <i class="fa fa-caret-down"></i></a>
+        <div class="dropdown-content">
+            <a href="vampire-survivors.html">Vampire Survivors Clone</a>
+            <a href="ray-tracing.html">Ray Tracing Renderer</a>
+            <a href="linked-plots.html">Linked Plots Package</a>
+            <a href="planet-impact.html">Planet Impact</a>
+            <a href="artic-angler.html">Arctic Angler</a>
+            <a href="inverse-kinematics.html">Inverse Kinematics</a>
+            <a href="stable-fluids.html">Fluid Simulation</a>
+            <a href="cloth-simulation.html">Cloth Simulation</a>
+            <a href="pathfinding-simulation.html">Pathfinding Simulation</a>
+            <a href="hanzi-dachu.html">汉字大厨</a>
+            <a href="ray-marching.html">Ray Marching</a>
+            <a href="hdr-bloom.html">HDR Shaders + Bloom</a>
+        </div>
     </div>
+</div>
 `;
-document.getElementById("header").innerHTML = appHeader;
+document.getElementById("new-header").innerHTML = appHeader;
 
 $(function() {
     $(".magic-tile").hover(
@@ -34,3 +46,10 @@ $(function() {
         }                         
     );                 
 }); 
+
+$(function () {
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height()/2);
+    });
+});
